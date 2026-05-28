561fa90f3071dd74f6d5b40461e88afefabecdd7
Starrah <starrah@foxmail.com>
2026-05-29 03:25:06 +0800

    !! 将项目名更改为VisualMaimai-Integration-MaiMuriDX。 原来的中键bug已经在新版本中修复了，所以原来的Fix Mod已经不需要了，就直接基于这个项目框架原地改成VisualMaimai-Integration-MaiMuriDX的新Mod。

--- Core.cs
+++ Core.cs
@@ -1,14 +1,10 @@
-﻿using System.Runtime.InteropServices;
-using EditorScene.Edit;
-using HarmonyLib;
+﻿using HarmonyLib;
 using MelonLoader;
-using UnityEngine;
-using UnityEngine.EventSystems;
 
-[assembly: MelonInfo(typeof(VisualMaimai_Fix.Core), "VM-TouchPad-MidButton-Fix", "1.0.0", "Starrah", null)]
+[assembly: MelonInfo(typeof(Starrah.VM_MaiMuri.Core), "VisualMaimai-Integration-MaiMuriDX", "2.0.0", "Starrah")]
 [assembly: MelonGame("CH3COOOH", "Visual Maimai")]
 
-namespace VisualMaimai_Fix;
+namespace Starrah.VM_MaiMuri;
 
 [HarmonyPatch]
 public class Core : MelonMod
@@ -16,48 +12,5 @@ public class Core : MelonMod
     public override void OnInitializeMelon()
     {
         MelonLogger.Msg("Initialized.");
-    }
-
-    [DllImport("user32.dll")]
-    static extern short GetAsyncKeyState(int vKey);
-
-    // 鼠标中键的虚拟键码是0x04
-    const int VK_MBUTTON = 0x04;
-
-    public override void OnUpdate()
-    {
-        // 检测中键是否按下，并且窗口聚焦
-        if (Application.isFocused && (GetAsyncKeyState(VK_MBUTTON) & 0x0001) != 0)
-        {
-            // 执行射线检测
-            GameObject clickedObject = GetClickedObject();
-            if (clickedObject != null)
-            {
-                EditMono editMono = clickedObject.GetComponentInParent<EditMono>();
-                if (editMono != null)
-                {
-                    // 模拟以中键手工调用editMono.OnPointerClick
-                    PointerEventData eventData = new PointerEventData(EventSystem.current);
-                    eventData.position = Input.mousePosition;
-                    eventData.button = PointerEventData.InputButton.Middle;
-                    editMono.OnPointerClick(eventData);
-                }
-            }
-        }
-    }
-
-    private GameObject GetClickedObject()
-    {
-        // 获取UI点击对象
-        PointerEventData eventData = new PointerEventData(EventSystem.current);
-        eventData.position = Input.mousePosition;
-        List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, results);
-        if (results.Count > 0)
-        {
-            return results[0].gameObject;
-        }
-
-        return null;
     }
 }
